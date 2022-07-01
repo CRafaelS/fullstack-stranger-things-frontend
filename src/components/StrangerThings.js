33a8164baf23d5b0fb93cdea2dfb398d57f04f79
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-07-01 08:54:32 -0300

    Variavel de ambiente para projeto em produção

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,6 +6,8 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+const { REACT_APP_DEVELOPER } = process.env;
+
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
@@ -115,6 +117,7 @@ class StrangerThings extends React.Component {
       >
         <div className="content strangerfy">
           <div className="change-reality">
+            {(REACT_APP_DEVELOPER === 'true') && <h1>Em desenvolvimento</h1>}
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
               Mudar de Realidade
